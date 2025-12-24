6f47175284143a73fed9e118e42779f2238a4345
Gerson-if <gersodecastro@gmail.com>
2025-12-24 15:38:09 -0400

    update in doc

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ESTE DOCUMENTO INCLUI ALTERAÇÕES E INCLUSÕES, EXPLICAÇÕES, DEFINIÇÕES DO CURSO FLASK
 - Na linha 1 do nosso programa principal, importamos o modulo flask e depois no import, importamos a classe Flask, os demais como url_for ou render_template são as funções da classe flask que invocamos em dado momento nas rotas que vamos criar.
+- No diretorio static estamos usando o bootstrap para estilização, nas paginas html linkamos ele com o url_for
--- app.py
+++ app.py
@@ -4,13 +4,12 @@ from flask import Flask, url_for, render_template, session, redirect
 secretkey = "minhasenhamuitodificil"
 app = Flask(__name__)
 
-@app.route('/inicio')
-def hello():
-    return "<h3> Bem vindo a minha Primeira aplicação flask </h3>"
 
 @app.route('/musicas')
 def listaMusicas():
-    return render_template('lista_musicas.html')
+    return render_template('lista_musicas.html', 
+                           titulo = 'conteudo da variavel')
+
 
 
 app.run(debug=True)
