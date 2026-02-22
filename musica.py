62e527a28092384aa90857ae8d332d3ca8bc0701
Gerson-if <gersodecastro@gmail.com>
2026-02-22 18:28:59 -0400

    criando pagina de login e autenticação

--- musica.py
+++ musica.py
@@ -2,15 +2,15 @@ from flask import Flask, url_for, render_template, redirect, request
 
 
 
-
-secretkey = "minhasenhamuitodificil"
-app = Flask(__name__)
-
+#declaração inicial de criação da aplicação, app recebe a classe Flask
+app = Flask(__name__) 
 
 
-class Musica: # criação de classe 
-    def __init__(self, nome, cantorBandaGrupo, genero): # o self referencia a propia classe 
-        self.nome = nome
+#criação de classe 
+class Musica:
+    #o self referencia a propia classe
+    def __init__(self, nome, cantorBandaGrupo, genero): 
+        self.nome = nome # classe ponto nome recebe nome, quando evocamos a classe passamos o nome para variavel responsavel.
         self.cantorBanda = cantorBandaGrupo
         self.genero = genero
         
@@ -20,32 +20,64 @@ musica01 = Musica('Temporal', 'Hungria', 'Rap')
 musica02 = Musica('Papai banca', 'Mc Ryan Sp', 'Funk')
 musica03 = Musica('Camissa 10', 'Turma do Pagode', 'Pagode')
 
-lista = [musica01, musica02, musica03] # aqui criamos uma lista dos nosso obejtos estanciados acima
+lista = [musica01, musica02, musica03]
+#aqui criamos uma lista dos nosso objetos instanciados acima
 
 
 
-
-@app.route('/') # nossa rota incial 
+#nossa rota incial
+@app.route('/')  
 def listarMusicas():
     return render_template('lista_musicas.html', 
                            titulo = 'conteudo da variavel',
                            musicas = lista)
 
 
+#aqui temos a rota de cadastro de musica
+# essa  rota devolve pra gente a template cadastrar_musica.htmls
 @app.route('/cadastrar')
 def cadastrarMusica():
     return render_template('cadastrar_musica.html')
 
 
+
+#essa rota é responsavel pelo cadastro da nova musica
+#passamos como parametro também o tipo de metodo que ela vai usar.
+#note que essa rota ela e respoavel por salvar e trasferir dados.
+#nesta classe tambem usamos o metodo post e nao metodo get, ou seja os dados nao sao passados por url e 
+# e sim sao passados de uma maneira mais segura.
 @app.route('/adicionar', methods=['POST', ])
 def adicionar_musica():
-    nome = request.form['txtNome'] # passa o valor de txtNome da pagina cadastrar_musica.html para a var nome
+    nome = request.form['txtNome'] 
+    #passa o valor de txtNome da pagina cadastrar_musica.html para a variavel nome
+    #esse passo de repete nas outras
     cantor = request.form['txtCantor']
     genero = request.form['txtGenero']
 
-    novaMusica = Musica(nome, cantor, genero) #cria nova musica
-    lista.append(novaMusica) #adiciona no fim da lista
+    #cria nova musica ou seja uma nova classe.
+    novaMusica = Musica(nome, cantor, genero)
+
+    #adiciona no fim da lista uma nova musica
+    lista.append(novaMusica) 
 
     return redirect('/')
+    #retorna a pagina inicial
 
+#rota de login inicial reponsavel por devolver a tela de login.
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+#rota de autenticação responsavel pela validação de dados da tela de login.
+@app.route('/autenticar', methods=['POST', ])
+def autenticar():
+    #se verdadeiro retorne para inicio '/'
+    if request.form['txtSenha'] == 'admin':
+        return redirect('/')
+    else:
+        # se falso retorne para login 
+        return redirect('/login')
+
+#declaração de execução da aplicação, com debug true sem necessidade de renicio para teste.
 app.run(debug=True)
